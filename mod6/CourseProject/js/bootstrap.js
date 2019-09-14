4ccf9bf61ef04cfe05f98f85147d392832b9785b
alyssafiess <49376722+alyssafiess@users.noreply.github.com>
2019-09-13 21:08:51 -0500

    Update

--- mod6/CourseProject/js/bootstrap.js
+++ mod6/CourseProject/js/bootstrap.js
@@ -16,6 +16,18 @@ if (typeof jQuery === 'undefined') {
   }
 }(jQuery);
 
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+function displayNotification() {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification('Hello world!');
+    });
+  }
+}
+
 /* ========================================================================
  * Bootstrap: transition.js v3.3.7
  * http://getbootstrap.com/javascript/#transitions
